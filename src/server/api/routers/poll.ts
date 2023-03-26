38a7782d35da51190cc68d166330f228f15c6f8f
Panji Gumelar <panjiggm@gmail.com>
2023-03-26 20:55:23 +0700

    adding the ability to view a poll

---
+++ src/server/api/routers/poll.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+
+export const pollRouter = createTRPCRouter({
+  getPoll: publicProcedure
+    .input(z.object({ pollId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.poll.findUnique({
+        where: {
+          id: input.pollId,
+        },
+        include: {
+          answers: true,
+        },
+      });
+    }),
+});
